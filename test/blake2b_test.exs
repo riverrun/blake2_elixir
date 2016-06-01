f20215350476e1ef8a93086f38f627f7da7948ef
David Whitlock <alovedalongthe@gmail.com>
2016-06-01 13:46:19 +0700

    Updated tests

--- test/blake2b_test.exs
+++ test/blake2b_test.exs
@@ -4,15 +4,26 @@ defmodule Blake2bTest do
   alias Blake2.Blake2b
 
   setup_all do
-    data = Path.join([__DIR__, "testvectors", "blake2b.csv"])
-            |> File.read!
-            |> :binary.split("\n", [:trim, :global])
-            |> Enum.map(&:binary.split(&1, ",", [:global]))
-    {:ok, %{data: data}}
+    blake2b = get_data "blake2b.csv"
+    blake2b_keyed = get_data "blake2b_keyed.csv"
+    {:ok, %{blake2b: blake2b, blake2b_keyed: blake2b_keyed}}
   end
 
-  test "blake2b test vectors", %{data: data} do
-    Enum.map data, fn [_, input, key, output] ->
+  def get_data(name) do
+    Path.join([__DIR__, "testvectors", name])
+    |> File.read!
+    |> :binary.split("\n", [:trim, :global])
+    |> Enum.map(&:binary.split(&1, ",", [:global]))
+  end
+
+  test "blake2b test vectors", %{blake2b: blake2b} do
+    Enum.map blake2b, fn [_, input, _, output] ->
+      assert Blake2b.hash(Base.decode16!(input, case: :lower), "") == output
+    end
+  end
+
+  test "blake2b keyed hash test vectors", %{blake2b_keyed: blake2b_keyed} do
+    Enum.map blake2b_keyed, fn [_, input, key, output] ->
       assert Blake2b.hash(Base.decode16!(input, case: :lower),
        Base.decode16!(key, case: :lower)) == output
     end
